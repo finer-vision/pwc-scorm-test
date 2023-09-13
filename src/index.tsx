61da499a67d353919e41b53e4cc119c2ac1c1661
Enijar <TheEnijar@gmail.com>
2023-09-13 12:33:56 +0100

    fix: init scorm

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter as Router } from "react-router-dom";
+import { useScorm } from "@/hooks/use-scorm";
 import App from "@/components/app/app";
 
 const rootElement = document.querySelector("#root");
@@ -8,6 +9,8 @@ const rootElement = document.querySelector("#root");
 if (rootElement === null) {
   throw new Error("No #root element found");
 }
+
+useScorm.getState().init();
 
 const root = ReactDOM.createRoot(rootElement);
 
